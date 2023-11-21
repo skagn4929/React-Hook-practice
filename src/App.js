d6dee584dd209aa74fc4718efdc17f7a8b68a5fa
기남후 <skagn4929@naver.com>
2023-11-21 18:55:46 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Timer from "./component/Timer";
 
+// ----------*/ useState 연습 /*------
+//
 // function App() {
 //   const [time, setTime] = useState(1);
 //   const handleClick = () => {
@@ -19,25 +22,83 @@ import { useState } from "react";
 //   );
 // }
 
+// function App() {
+//   const [names, setNames] = useState(["홍길동", "김민수"]);
+//   const [input, setInput] = useState("");
+//   const handleInputChange = (e) => {
+//     setInput(e.target.value);
+//   };
+//   const handleUpload = () => {
+//     setNames((prev) => {
+//       console.log("이전스테이트: ", prev);
+//       return [input, ...prev];
+//     });
+//   };
+//   return (
+//     <div>
+//       <input type="text" value={input} onChange={handleInputChange} />
+//       <button onClick={handleUpload}>Upload</button>
+//       {names.map((name, idx) => {
+//         return <p key={idx}>{name}</p>;
+//       })}
+//     </div>
+//   );
+// }
+// ----------------------------------------------------------------
+
+// ----------*/ useEffect 연습 /*------
+//
+// function App() {
+//   const [count, setCount] = useState(1);
+//   const [name, setName] = useState("");
+//   const handleCountUpdate = () => {
+//     setCount(count + 1);
+//   };
+//   const handleInputChange = (e) => {
+//     setName(e.target.value);
+//   };
+// 렌더링 될때마다 매번 실행됨.
+// useEffect(() => {
+//   console.log("렌더링");
+// });
+//
+// 마운팅 + count가 변화할때마다 실행됨.
+// useEffect(() => {
+//   console.log("count 변화");
+// }, [count]);
+//
+// 마운팅 + name이 변경될때마다 실행됨.
+// useEffect(() => {
+//   console.log("name 변화");
+// }, [name]);
+//
+// 마운팅 될때만 실행됨.
+// useEffect(() => {
+//   console.log("마운팅");
+// }, []);
+//   return (
+//     <div>
+//       <button onClick={handleCountUpdate}>Update</button>
+//       <span>count: {count}</span>
+//       <br />
+//       <input type="text" value={name} onChange={handleInputChange} />
+//       <span>name: {name}</span>
+//     </div>
+//   );
+// }
+
 function App() {
-  const [names, setNames] = useState(["홍길동", "김민수"]);
-  const [input, setInput] = useState("");
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-  const handleUpload = () => {
-    setNames((prev) => {
-      console.log("이전스테이트: ", prev);
-      return [input, ...prev];
-    });
-  };
+  const [showTimer, setshowTimer] = useState(false);
   return (
     <div>
-      <input type="text" value={input} onChange={handleInputChange} />
-      <button onClick={handleUpload}>Upload</button>
-      {names.map((name, idx) => {
-        return <p key={idx}>{name}</p>;
-      })}
+      {showTimer && <Timer />}
+      <button
+        onClick={() => {
+          setshowTimer(!showTimer);
+        }}
+      >
+        Toggle Timer
+      </button>
     </div>
   );
 }
