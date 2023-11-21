75a8e3e19417834b85eea14e8550d3ad60f9bebf
기남후 <skagn4929@naver.com>
2023-11-21 18:21:35 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// function App() {
+//   const [time, setTime] = useState(1);
+//   const handleClick = () => {
+//     let newTime;
+//     if (time >= 12) {
+//       newTime = 1;
+//     } else {
+//       newTime = time + 1;
+//     }
+//     setTime(newTime);
+//   };
+//   return (
+//     <div className="App">
+//       <span>현재 시각: {time}시</span>
+//       <button onClick={handleClick}>Update</button>
+//     </div>
+//   );
+// }
 
 function App() {
+  const [names, setNames] = useState(["홍길동", "김민수"]);
+  const [input, setInput] = useState("");
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleUpload = () => {
+    setNames((prev) => {
+      console.log("이전스테이트: ", prev);
+      return [input, ...prev];
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{name}</p>;
+      })}
     </div>
   );
 }
